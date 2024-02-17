8e15096263357a716d4caed3dd57a624850f5d01
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-17 08:50:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+jwplayer("video").setup({
+  width: "100%",
+  height: "100%",
+  autostart: true,
+  mute: false,
+  file: "https://prod-sports-hin-fa.jiocinema.com/bpk-tv/Colors_Cineplex_voot_MOB/Fallback/Colors_Cineplex_voot_MOB.m3u8"
+});
