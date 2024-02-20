3d1103f7cef62b04301243fd175f8aa359e59268
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-20 20:24:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-// Set the source for the iframe
-document.getElementById('streamIframe').src = 'https://finscorpio.com/stream/ch1.php';
-
-// Your existing script logic can go here
-document.getElementById('closeBtn').addEventListener('click', function() {
-    document.getElementById('formContainer').style.display = 'none';
-    document.getElementById('logo').style.display = 'block';
+jwplayer("video").setup({
+  width: "100%",
+  height: "100%",
+  autostart: true,
+  mute: false,
+  file: "https://tencentscdn2.tamashaweb.com/v1/018ad584681c09831f17035dc819/018ad58586961eb401a60363c44c/main.m3u8"
 });
-
-function openForm() {
-    document.getElementById('formContainer').style.display = 'block';
-    document.getElementById('logo').style.display = 'none';
-}
-
-var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
-    backdrop: 'static'
-});
-myModal.show();
